a16ae8ffd2eaa896bfa03e60cd4e54d4ba6d320e
Jeff Panici <jeffpanici75@gmail.com>
2021-03-25 08:25:24 -0600

    tidy up stable_array_t

--- include/basecode/core/stable_array.h
+++ include/basecode/core/stable_array.h
@@ -19,130 +19,162 @@
 #pragma once
 
 #include <cassert>
-#include <cstring>
-#include <algorithm>
 #include <basecode/core/types.h>
-#include <basecode/core/slice.h>
-#include <basecode/core/memory.h>
 #include <basecode/core/context.h>
 #include <basecode/core/iterator.h>
 #include <basecode/core/memory/system/slab.h>
 
 namespace basecode {
+    template <typename T>
+    concept Stable_Array = requires(const T& t) {
+        typename                T::Value_Type;
+
+        {t.alloc}               -> same_as<alloc_t*>;
+        {t.slab}                -> same_as<alloc_t*>;
+        {t.data}                -> same_as<typename T::Value_Type**>;
+        {t.size}                -> same_as<u32>;
+        {t.capacity}            -> same_as<u32>;
+    };
+
     template<typename T>
     struct stable_array_t final {
-        T**                     data;
+        using                   Value_Type = T;
+
         alloc_t*                alloc;
-        alloc_t*                slab_alloc;
+        alloc_t*                slab;
+        Value_Type**            data;
         u32                     size;
         u32                     capacity;
 
-        T& operator[](u32 index)                            { return *data[index];      }
-        const T& operator[](u32 index) const                { return *data[index];      }
+        Value_Type& operator[](u32 index)                               { return *data[index];      }
+        const Value_Type& operator[](u32 index) const                   { return *data[index];      }
 
         struct iterator_state_t {
             u32                 pos;
-            inline T* get(stable_array_t* ref)              { return ref->data[pos];    }
-            inline u0 end(const stable_array_t* ref)        { pos = ref->size;          }
-            inline u0 next(const stable_array_t* ref)       { ++pos;                    }
-            inline u0 begin(const stable_array_t* ref)      { pos = 0;                  }
-            inline b8 cmp(const iterator_state_t& s) const  { return pos != s.pos;      }
-            inline const T* get(const stable_array_t* ref)  { return ref->data[pos];    }
+            inline Value_Type* get(stable_array_t* ref)                 { return ref->data[pos];    }
+            inline u0 end(const stable_array_t* ref)                    { pos = ref->size;          }
+            inline u0 next(const stable_array_t* ref)                   { UNUSED(ref); ++pos;       }
+            inline u0 begin(const stable_array_t* ref)                  { UNUSED(ref); pos = 0;     }
+            inline b8 cmp(const iterator_state_t& s) const              { return pos != s.pos;      }
+            inline const Value_Type* get(const stable_array_t* ref)     { return ref->data[pos];    }
         };
-        DECL_ITERS(stable_array_t, T*, iterator_state_t);
+        DECL_ITERS(stable_array_t, Value_Type*, iterator_state_t);
     };
 
     namespace stable_array {
-        template<typename T> u0 clear(stable_array_t<T>& array);
-        template<typename T> u0 reserve(stable_array_t<T>& array, u32 new_capacity);
-        template<typename T> u0 grow(stable_array_t<T>& array, u32 new_capacity = 8);
-        template<typename T> stable_array_t<T> make(alloc_t* alloc = context::top()->alloc);
-        template<typename T> u0 init(stable_array_t<T>& array, alloc_t* alloc = context::top()->alloc, u8 num_pages = 1);
-        template<typename T> stable_array_t<T> make(std::initializer_list<T> elements, alloc_t* alloc = context::top()->alloc);
+        template <Stable_Array T>
+        u0 grow(T& array, u32 new_capacity = 16);
 
-        template<typename T> u0 pop(stable_array_t<T>& array) {
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        u0 reserve(T& array, u32 new_capacity);
+
+        template<Stable_Array T>
+        u0 pop(T& array) {
             if (array.size == 0) return;
-            memory::free(array.slab_alloc, array.data[array.size - 1]);
+            memory::free(array.slab, array.data[array.size - 1]);
             --array.size;
         }
 
-        template<typename T> u0 free(stable_array_t<T>& array) {
-            clear(array);
-        }
-
-        template<typename T> u0 trim(stable_array_t<T>& array) {
-            reserve(array, array.size);
-        }
-
-        template<typename T> T* back(stable_array_t<T>& array) {
-            return array.size == 0 ? nullptr : array.data[array.size - 1];
-        }
-
-        template<typename T> u0 reset(stable_array_t<T>& array) {
-            array.size = {};
-            memory::slab::reset(array.slab_alloc);
-            std::memset(array.data, 0, array.capacity * sizeof(T*));
-        }
-
-        template<typename T> u0 clear(stable_array_t<T>& array) {
+        template <Stable_Array T>
+        u0 clear(T& array) {
             memory::free(array.alloc, array.data);
-            memory::system::free(array.slab_alloc);
+            memory::system::free(array.slab);
             array.data = {};
             array.size = array.capacity = {};
         }
 
-        template<typename T> b8 empty(stable_array_t<T>& array) {
+        template <Stable_Array T>
+        u0 free(T& array) {
+            clear(array);
+        }
+
+        template <Stable_Array T>
+        u0 trim(T& array) {
+            reserve(array, array.size);
+        }
+
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        u0 reset(T& array) {
+            array.size = {};
+            memory::slab::reset(array.slab);
+            std::memset(array.data, 0, array.capacity * sizeof(Value_Type*));
+        }
+
+        template <Stable_Array T>
+        b8 empty(T& array) {
             return array.size == 0;
         }
 
-        template<typename T> T* front(stable_array_t<T>& array) {
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        Value_Type* back(T& array) {
+            return array.size == 0 ? nullptr : array.data[array.size - 1];
+        }
+
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        Value_Type* front(T& array) {
             return array.size == 0 ? nullptr : array.data[0];
         }
 
-        template<typename T> T& append(stable_array_t<T>& array) {
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        Value_Type& append(T& array) {
             if (array.size + 1 > array.capacity)
                 grow(array);
-            T* v = (T*) memory::alloc(array.slab_alloc);
+            auto v = (Value_Type*) memory::alloc(array.slab);
             array.data[array.size++] = v;
             return *v;
         }
 
-        template<typename T> stable_array_t<T> make(alloc_t* alloc) {
-            stable_array_t<T> array;
-            init(array, alloc);
-            return array;
-        }
-
-        template<typename T> u0 erase(stable_array_t<T>& array, u32 index) {
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        u0 erase(T& array, u32 index) {
             if (index >= array.size)
                 return;
-            memory::free(array.slab_alloc, array.data[index]);
+            memory::free(array.slab, array.data[index]);
             auto dest = array.data + index;
-            std::memcpy(dest, dest + 1, (array.size - index) * sizeof(T*));
+            std::memcpy(dest,
+                        dest + 1,
+                        (array.size - index) * sizeof(Value_Type*));
             --array.size;
         }
 
-        template<typename T> u0 append(stable_array_t<T>& array, T&& value) {
-            if (array.size + 1 > array.capacity)
-                grow(array);
-            auto v = (T*) memory::alloc(array.slab_alloc);
-            *v = value;
-            array.data[array.size++] = v;
-        }
-
-        template<typename T> u0 truncate(stable_array_t<T>& array, u32 index) {
-            for (u32 i = index; i < array.size; ++i)
-                memory::free(array.slab_alloc, array.data[i]);
-            array.size = index;
-        }
-
-        template<typename T> u0 resize(stable_array_t<T>& array, u32 new_size) {
+        template <Stable_Array T>
+        u0 resize(T& array, u32 new_size) {
             if (new_size > array.capacity)
                 grow(array, new_size);
             array.size = new_size;
         }
 
-        template<typename T> b8 erase(stable_array_t<T>& array, const T* value) {
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        u0 append(T& array, Value_Type&& value) {
+            if (array.size + 1 > array.capacity)
+                grow(array);
+            auto v = (Value_Type*) memory::alloc(array.slab);
+            *v = value;
+            array.data[array.size++] = v;
+        }
+
+        template <Stable_Array T>
+        u0 truncate(T& array, u32 index) {
+            for (u32 i = index; i < array.size; ++i)
+                memory::free(array.slab, array.data[i]);
+            array.size = index;
+        }
+
+        template <Stable_Array T>
+        u0 grow(T& array, u32 new_capacity) {
+            new_capacity = std::max(new_capacity, array.capacity);
+            reserve(array, new_capacity * 2 + 8);
+        }
+
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        b8 erase(T& array, const Value_Type* value) {
             s32 idx = -1;
             for (u32 i = 0; i < array.size; ++i) {
                 if (array.data[i] == value) {
@@ -151,37 +183,17 @@ namespace basecode {
                 }
             }
             if (idx == -1) return false;
-            memory::free(array.slab_alloc, array.data[idx]);
+            memory::free(array.slab, array.data[idx]);
             --array.size;
             auto dest = array.data + idx;
-            std::memcpy(dest, dest + 1, (array.size - idx) * sizeof(T*));
+            std::memcpy(dest,
+                        dest + 1,
+                        (array.size - idx) * sizeof(Value_Type*));
             return true;
         }
 
-        template<typename T> u0 grow(stable_array_t<T>& array, u32 new_capacity) {
-            new_capacity = std::max(new_capacity, array.capacity);
-            reserve(array, new_capacity * 2 + 8);
-        }
-
-        template<typename T> u0 append(stable_array_t<T>& array, const T& value) {
-            if (array.size + 1 > array.capacity)
-                grow(array);
-            T* v = (T*) memory::alloc(array.slab_alloc);
-            *v = value;
-            array.data[array.size++] = v;
-        }
-
-        template<typename T> s32 contains(stable_array_t<T>& array, const T* value) {
-            for (u32 i = 0; i < array.size; ++i) {
-                if (array.data[i] == value)
-                    return i;
-            }
-            return -1;
-        }
-
-        template<typename T> u0 reserve(stable_array_t<T>& array, u32 new_capacity) {
-            assert(array.alloc);
-
+        template <Stable_Array T, typename Value_Type>
+        u0 reserve(T& array, u32 new_capacity) {
             if (new_capacity == array.capacity)
                 return;
 
@@ -192,53 +204,96 @@ namespace basecode {
                 return;
             } else if (new_capacity < array.capacity) {
                 for (u32 i = new_capacity - 1; i < array.capacity; ++i)
-                    memory::free(array.slab_alloc, array.data[i]);
+                    memory::free(array.slab, array.data[i]);
             }
 
             new_capacity = std::max(array.size, new_capacity);
-            array.data = (T**) memory::realloc(array.alloc, array.data, new_capacity * sizeof(T*), alignof(T*));
+            array.data = (Value_Type**) memory::realloc(array.alloc,
+                                                       array.data,
+                                                       new_capacity * sizeof(Value_Type*),
+                                                       alignof(Value_Type*));
             const auto data = array.data + array.size;
             const auto size_to_clear = new_capacity > array.capacity ? new_capacity - array.capacity : 0;
-            std::memset(data, 0, size_to_clear * sizeof(T*));
+            std::memset(data, 0, size_to_clear * sizeof(Value_Type*));
             array.capacity = new_capacity;
         }
 
-        template<typename T> u0 insert(stable_array_t<T>& array, u32 index, T&& value) {
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        u0 append(T& array, const Value_Type& value) {
             if (array.size + 1 > array.capacity)
                 grow(array);
-            if (index < array.size)
-                std::memmove(array.data + index + 1, array.data + index, (array.size - index) * sizeof(T*));
-            T* v = (T*) memory::alloc(array.slab_alloc);
+            auto v = (Value_Type*) memory::alloc(array.slab);
+            *v = value;
+            array.data[array.size++] = v;
+        }
+
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        s32 contains(T& array, const Value_Type* value) {
+            for (u32 i = 0; i < array.size; ++i) {
+                if (array.data[i] == value)
+                    return i;
+            }
+            return -1;
+        }
+
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        u0 insert(T& array, u32 index, Value_Type&& value) {
+            if (array.size + 1 > array.capacity)
+                grow(array);
+            if (index < array.size) {
+                std::memmove(array.data + index + 1,
+                             array.data + index,
+                             (array.size - index) * sizeof(Value_Type*));
+            }
+            auto v = (Value_Type*) memory::alloc(array.slab);
             *v = value;
             array.data[index] = v;
             ++array.size;
         }
 
-        template<typename T> u0 insert(stable_array_t<T>& array, u32 index, const T& value) {
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        u0 insert(T& array, u32 index, const Value_Type& value) {
             if (array.size + 1 > array.capacity)
                 grow(array);
-            if (index < array.size)
-                std::memmove(array.data + index + 1, array.data + index, (array.size - index) * sizeof(T*));
-            T* v = (T*) memory::alloc(array.slab_alloc);
+            if (index < array.size) {
+                std::memmove(array.data + index + 1,
+                             array.data + index,
+                             (array.size - index) * sizeof(Value_Type*));
+            }
+            auto v = (Value_Type*) memory::alloc(array.slab);
             *v = value;
             array.data[index] = v;
             ++array.size;
         }
 
-        template<typename T> u0 init(stable_array_t<T>& array, alloc_t* alloc, u8 num_pages) {
+        template <Stable_Array T,
+                  typename Value_Type = typename T::Value_Type>
+        u0 init(T& array, alloc_t* alloc = context::top()->alloc, u8 num_pages = 1) {
             array.data  = {};
             array.alloc = alloc;
             array.size  = array.capacity = {};
 
             slab_config_t slab_config{};
             slab_config.backing   = array.alloc;
-            slab_config.buf_size  = sizeof(T);
-            slab_config.buf_align = alignof(T);
+            slab_config.buf_size  = sizeof(Value_Type);
+            slab_config.buf_align = alignof(Value_Type);
             slab_config.num_pages = num_pages;
-            array.slab_alloc      = memory::system::make(alloc_type_t::slab, &slab_config);
+            array.slab            = memory::system::make(alloc_type_t::slab, &slab_config);
         }
 
-        template<typename T> stable_array_t<T> make(std::initializer_list<T> elements, alloc_t* alloc) {
+        template <typename T>
+        stable_array_t<T> make(alloc_t* alloc = context::top()->alloc) {
+            stable_array_t<T> array;
+            init(array, alloc);
+            return array;
+        }
+
+        template <typename T>
+        stable_array_t<T> make(std::initializer_list<T> elements, alloc_t* alloc = context::top()->alloc) {
             stable_array_t<T> array;
             init(array, alloc);
             reserve(array, elements.size());
