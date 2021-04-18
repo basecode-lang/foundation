1369bb6ea5624901d996eac5b0bef74b3ed10576
Jeff Panici <jeffpanici75@gmail.com>
2021-04-18 14:07:08 -0600

    macro: sketch some ideas for macro system

---
+++ include/basecode/core/macro.h
@@ -0,0 +1,104 @@
+// ----------------------------------------------------------------------------
+// ____                               _
+// |  _\                             | |
+// | |_)| __ _ ___  ___  ___ ___   __| | ___ TM
+// |  _< / _` / __|/ _ \/ __/ _ \ / _` |/ _ \
+// | |_)| (_| \__ \  __/ (_| (_) | (_| |  __/
+// |____/\__,_|___/\___|\___\___/ \__,_|\___|
+//
+//      F O U N D A T I O N   P R O J E C T
+//
+// Copyright (C) 2020 Jeff Panici
+// All rights reserved.
+//
+// This software source file is licensed under the terms of MIT license.
+// For details, please read the LICENSE file.
+//
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include <basecode/core/types.h>
+#include <basecode/core/array.h>
+
+namespace basecode {
+    struct macro_t;
+
+    using macro_array_t         = array_t<macro_t*>;
+
+    enum class macro_op_t : u8 {
+        nop,
+        get,
+        set,
+        cmp,
+        je,
+        jne,
+        jl,
+        jle,
+        jg,
+        jge,
+        copy,
+    };
+
+    enum class macro_directive_t : u8 {
+        none,
+        if_,
+        odd,
+        each,
+        loop,
+        last,
+        even,
+        first,
+        local,
+        after,
+        macro,
+        upper,
+        lower,
+        label,
+        global,
+        substr,
+        escape,
+        break_,
+        binary,
+        before,
+        between,
+        nothing,
+        include,
+        for_each,
+        continue_,
+        after_all,
+        before_all,
+    };
+
+    struct macro_t final {
+        u32                     id;
+    };
+
+    //  .macro(quux, n: int, f: bool, lhs: ast, rhs: ast) {
+    //      .local(i: int)
+    //      .loop(0 .. n : i) {
+    //          .before-all(f)  { "Let's get ready to RUMBLE!!!\n" }
+    //          .each           { lhs && " is " && rhs             }
+    //          .even           { "------------------\n"           }
+    //          .odd            { "==================\n"           }
+    //      }
+    //  }
+    //
+    //  Hello, ${first_name}!
+    //
+    //  .label(left) This special macro quux, does something .label(right) funny:
+    //  ${quux(3, true, left, right)}.
+    //
+    namespace macro {
+        enum class status_t : u32 {
+            ok,
+            error
+        };
+
+        namespace system {
+            u0 fini();
+
+            status_t init(alloc_t* alloc = context::top()->alloc);
+        }
+    }
+}
---
+++ src/libcore/macro.cpp
@@ -0,0 +1,37 @@
+// ----------------------------------------------------------------------------
+// ____                               _
+// |  _\                             | |
+// | |_)| __ _ ___  ___  ___ ___   __| | ___ TM
+// |  _< / _` / __|/ _ \/ __/ _ \ / _` |/ _ \
+// | |_)| (_| \__ \  __/ (_| (_) | (_| |  __/
+// |____/\__,_|___/\___|\___\___/ \__,_|\___|
+//
+//      F O U N D A T I O N   P R O J E C T
+//
+// Copyright (C) 2020 Jeff Panici
+// All rights reserved.
+//
+// This software source file is licensed under the terms of MIT license.
+// For details, please read the LICENSE file.
+//
+// ----------------------------------------------------------------------------
+
+#include <basecode/core/macro.h>
+
+namespace basecode::macro {
+    struct macro_system_t final {
+        alloc_t*                alloc;
+    };
+
+    thread_local macro_system_t t_macro_sys{};
+
+    namespace system {
+        u0 fini() {
+        }
+
+        status_t init(alloc_t* alloc) {
+            t_macro_sys.alloc = alloc;
+            return status_t::ok;
+        }
+    }
+}
